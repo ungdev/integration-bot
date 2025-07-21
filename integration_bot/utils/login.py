2ac1b9ce0158492ae55b3a05c085cb56de24afe4
Antoine D <antoine.dufils@yahoo.fr>
2025-07-21 23:33:31 +0900

    fix: token printing and https warning

--- integration_bot/utils/login.py
+++ integration_bot/utils/login.py
@@ -6,6 +6,6 @@ def login() -> str:
   payload:dict = {"email": os.getenv('BOT_WEBSITE_EMAIL'), "password": os.getenv('BOT_WEBSITE_PASSWORD')}
   response = requests.post(api_url, json=payload, verify=False)
   if response.status_code == 200:
-    return response.json().get('token', '')
+    return response.json().get('data', {}).get('token', '')
   else:
     raise Exception(f"Login failed with status code {response.status_code}: {response.text}")
